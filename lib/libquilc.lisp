8e04fd21c4d16a393a52048d0e4028670a28154d
Kartik Singh <kssingh@hrl.com>
2022-01-28 16:31:13 -0800

    Add remaining exports

--- lib/libquilc.lisp
+++ lib/libquilc.lisp
@@ -25,6 +25,9 @@
   (format t "~a qubits, ~a links~%" (cl-quil::chip-spec-n-qubits chip-spec) (cl-quil::chip-spec-n-links chip-spec))
   (format t "~%links:~%~{~/quilc:print-link/~%~}~%" (coerce (cl-quil::chip-spec-links chip-spec) 'list)))
 
+(defun compile-protoquil (parsed-program chip-specification)
+  (compiler-hook parsed-program chip-specification :protoquil t))
+
 (sbcl-librarian:define-library libquilc (:error-map error-map
                                          :function-linkage "QUILC_API"
                                          :function-prefix "quilc_")
@@ -35,8 +38,9 @@
    (("parse_quil" safely-parse-quil) quil-program ((source :string)))
    (("print_program" print-program) :void ((program quil-program)))
    (("compile_quil" compiler-hook) quil-program ((program quil-program) (chip-spec chip-specification)))
+   (("compile_protoquil" compile-protoquil) quil-program ((program quil-program) (chip-spec chip-specification)))
    (("build_nq_linear_chip" cl-quil::build-nq-linear-chip) chip-specification ((n :int)))
-   (("chip_spec_from_file" quil::read-chip-spec-file) chip-specification ((filename :string)))
+   (("chip_spec_from_isa_descriptor" lookup-isa-descriptor-for-name) chip-specification ((descriptor :string)))
    (("print_chip_spec" print-chip-spec) :void ((chip-spec chip-specification)))))
 
 (sbcl-librarian:build-bindings libquilc ".")
