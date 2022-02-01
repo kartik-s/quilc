8ac8415514752dd133164a6c903244a2a3ce790c
Kartik Singh <kssingh@hrl.com>
2022-02-01 10:11:43 -0800

    Add build + first C test

--- lib/tests/c-tests.lisp
+++ lib/tests/c-tests.lisp
@@ -11,8 +11,18 @@
       (uiop:run-program "make")
     (is (eql exit-code 0))))
 
+(deftest test-build-c-tests ()
+  "Test that the C tests build."
+  (uiop:chdir "tests/c")
+  (multiple-value-bind (output error-output exit-code)
+      (uiop:run-program "make")
+    (is (eql exit-code 0))))
+
 (deftest test-compile-quil ()
-  (is t))
+  "Test compiling Quil programs."
+  (multiple-value-bind (output error-output exit-code)
+      (uiop:run-program "tests/c/compile-quil" :input '("H 0") :output :string)
+    (is (eql exit-code 0))))
 
 (deftest test-compile-protoquil ()
   (is t))
