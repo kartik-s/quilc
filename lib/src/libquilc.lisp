14e77b9f3b742834e373d318045565eeb89cb257
Kartik Singh <kssingh@hrl.com>
2022-02-01 12:35:06 -0800

    Refactor chip spec printing

--- lib/src/libquilc.lisp
+++ lib/src/libquilc.lisp
@@ -14,17 +14,6 @@
 (sbcl-librarian:define-error-map error-map error-type 0
   (t (condition) (declare (ignore condition)) 1))
 
-(defun print-link (stream link &optional colon-p at-sign-p)
-  (declare (ignore colon-p at-sign-p))
-  (let* ((cxns (cl-quil::hardware-object-cxns link))
-         (qubit0 (cl-quil::vnth 0 (cl-quil::vnth 0 cxns)))
-         (qubit1 (cl-quil::vnth 1 (cl-quil::vnth 0 cxns))))
-    (format stream "~a -- ~a" qubit0 qubit1)))
-
-(defun print-chip-spec (chip-spec)
-  (format t "~a qubits, ~a links~%" (cl-quil::chip-spec-n-qubits chip-spec) (cl-quil::chip-spec-n-links chip-spec))
-  (format t "~%links:~%~{~/quilc:print-link/~%~}~%" (coerce (cl-quil::chip-spec-links chip-spec) 'list)))
-
 (defun compile-protoquil (parsed-program chip-specification)
   (compiler-hook parsed-program chip-specification :protoquil t))
 
@@ -41,7 +30,7 @@
    (("compile_protoquil" compile-protoquil) quil-program ((program quil-program) (chip-spec chip-specification)))
    (("build_nq_linear_chip" cl-quil::build-nq-linear-chip) chip-specification ((n :int)))
    (("chip_spec_from_isa_descriptor" lookup-isa-descriptor-for-name) chip-specification ((descriptor :string)))
-   (("print_chip_spec" print-chip-spec) :void ((chip-spec chip-specification)))))
+   (("print_chip_spec" cl-quil::debug-print-chip-spec) :void ((chip-spec chip-specification)))))
 
 (sbcl-librarian:build-bindings libquilc ".")
 (sbcl-librarian:build-python-bindings libquilc ".")
